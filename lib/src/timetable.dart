1976d750289c55760c9799aa472f1326707d15de
Techno-Disaster <nitinnirve@gmail.com>
2019-08-25 14:32:50 +0530

    Update TT

--- lib/src/timetable.dart
+++ lib/src/timetable.dart
@@ -2,17 +2,18 @@ const List<String> mon = [
   'E1 PIC LAB B110 | E2 PIC LAB B110 | E3 PIC LAB B214',
   'EP A102 SAM',
   'AM A102 VEK',
-  'EM A102',
+  'EM A102 BSS',
 ];
 const List<String> tues = [
   'E1 AM A102 | E2 AM A103 | E3 AM B104',
-  'EP A102 | E2 PIC B110 | E3 PIC B213',
+  'EP A102 A_S',
+  'E1 PIC B110| E2 PIC B110 | E3 PIC B213',
   'AM A102 RVD',
 ];
 const List<String> wed = [
   'E1 EP A016 | E2 EVS A102 | E3 EM A207',
-  'EM A102',
-  'AM A102 A_S',
+  'EM A102 BSS',
+  'AM A102 VEK',
   'EP A102 A_S'
 ];
 const List<String> thur = [
